159fe1ac4f2e2da6d9c49eb7acd2a7116a200101
jananividhya <jananividhya123@gmail.com>
2019-03-19 18:24:35 +0530

    first component 1 commit

---
+++ src/components/RegisterComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Register = () => {
+    return (
+        <div>
+            <h2>Header</h2>
+            <p>Constant data on the top of all pages or parts of the application</p>
+        </div>
+    );
+}
+
+export default Register;
