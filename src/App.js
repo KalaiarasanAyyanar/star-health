50dcb76f84b51299d80a5ee5a184b342f00a11f8
jananividhya <jananividhya123@gmail.com>
2019-03-19 16:56:57 +0530

    skeleton

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="totaldiv">
+      <div className="App">
+ <div className="split">
+      <div className="left">
+  <div className="centered">
+    <h2>Header</h2>
+    <p>Constant data on the top of all pages or parts of the application</p>
+  </div>
+</div>
+<div className="  left1">
+  <div className="centered">
+   
+    <h2>Registering a plan for family</h2>
+        <p>A step by step process with a meaningful UI for better understanding</p>
+ </div>
+ </div>
+</div>
+
+<div className="  right">
+  <div className="centered">
+   
+    <h2>Sidebar carousel</h2>
+    <p>Displays promotional information about star health</p>
+  </div>
+</div>
+      </div>
+      <div className="  left3">
+  <div className="centered">
+   
+    <h2>Data about the insurance </h2>
+        <p>Contains image and divisions of data</p>
+ </div>
+ </div>
+ <div className="  left4">
+  <div className="centered">
+   
+  <h2>Data about the insurance-2 </h2>
+        <p>Contains image and divisions of data</p>
+ </div>
+ </div>
+ <div className="  left5">
+  <div className="centered">
+   
+  <h2>Footer</h2>
+    <p>Constant data on the bottom of across all pages or parts of the application</p>
+ </div>
+ </div>
+      </div>
+    );
+  }
+}
+
+export default App;
