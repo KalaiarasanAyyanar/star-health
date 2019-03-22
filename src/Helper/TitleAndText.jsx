a49c2f30ad43cfd44987648b4cea7ce87cf68f76
alagammailakshmanan <alagammai.l@purpleslate.in>
2019-03-22 10:06:51 +0530

    hello

--- src/Helper/TitleAndText.jsx
+++ src/Helper/TitleAndText.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const TitleAndText = (props) => {
     return (
         <div className="title-and-text">
-            <div className="title-and-text--title">
+            <div className="title-and-text--title" style={{fontSize:"24px"}}>
                 {props.title}
             </div>
             {
                 (props.breifData) &&
-                    <div className="title-and-text--text-breifData">
+                    <div className="title-and-text--text-breifData" style={{ fontSize: "20px", fontWeight: "500", color: "black" }}>
                         {props.breifData}
                     </div>
                 
@@ -17,7 +17,7 @@ const TitleAndText = (props) => {
                 {
                     props.textArray.map((text,key)=>{
                         return(
-                            <div key={key} className="title-and-text--text">
+                            <div key={key} className="title-and-text--text" style={{ fontSize: "17px", fontWeight: "400", color: "black" }}>
                                 {text}
                             </div>
                         );
